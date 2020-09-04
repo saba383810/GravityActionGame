7e8ce8792a50b21c301268ecabc021ad72e406b3
saba383810 <saba383810@gmail.com>
2020-09-05 00:28:40 +0900

    全体修正

---
+++ GravityAction/Assets/scripts/ClickScripts.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+    public void gameSelectButtonClick()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void startButtonClick()
+    {
+        SceneManager.LoadScene("Start");
+    }
+    public void Stage1ButtonClick()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+    public void Stage2ButtonClick()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+    public void Stage3ButtonClick()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+    public void Stage4ButtonClick()
+    {
+        SceneManager.LoadScene("Stage4");
+    }
+}
---
+++ GravityAction/Assets/scripts/PlayerMove.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private bool isReverse;
+    public GameObject player;
+    Animator animPlayer;
+    private bool reachFlag = false;
+    public bool isdamege = false;
+    private bool invokeCheck = false;
+
+    void Start()
+    {
+        animPlayer = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (isdamege == false)
+        {
+            transform.position += new Vector3(-0.15f, 0, 0);
+            invokeCheck = false;
+        }
+        else
+        {
+            if (invokeCheck == false)
+            {
+                Invoke("setFalse", 4.2f);
+                invokeCheck = true;
+            }
+        }
+
+        //天井か地面についていないと重力反転できないようにする。
+        Gravity grav = GetComponent<Gravity>();
+        if (Input.GetKeyDown(KeyCode.Space) && reachFlag == true) {
+            if (isReverse == true)
+            {
+                animPlayer.SetBool("isFloating", false);
+                grav.setFalse();
+                isReverse = false;  
+            }
+            else
+            {
+                animPlayer.SetBool("isFloating", true);
+                grav.setTrue();
+                isReverse = true;
+            }
+        }
+    }
+	private void OnCollisionEnter(Collision col)
+	{
+        if (col.gameObject.tag == "Ground")
+        {
+            reachFlag = true;
+        }
+	}
+	private void OnCollisionExit(Collision col)
+	{
+        if (col.gameObject.tag == "Ground")
+        {
+            reachFlag = false;
+        }
+	}
+
+    public void setTrue()
+    {
+        isdamege = true;
+    }
+    public void setFalse()
+    {
+        animPlayer.SetBool("isCollision", false);
+        isdamege = false;
+    }
+}
